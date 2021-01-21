39ef2849b91671f4c40a36026d1d6e16f087afac
njmaeff <nik@jmaeff.me>
2021-01-21 10:47:56 -0800

    fix(website-ui): change outside container to absolute to fix scrolling on iphone

--- src/website-ui/components/layout/containerOuter.tsx
+++ src/website-ui/components/layout/containerOuter.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const ContainerOuter = styled.div`
+    position: absolute;
     background-color: ${({theme}) => theme.palette.light};
     overflow:auto;
+    width: 100%;
+    height: 100%;
 `;
